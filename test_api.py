02d912214b008bb5662cb466bbab2a6fa0c8af0f
Bir Bahadur Choudhary <birbahadur.19jdds028@jietjodhpur.ac.in>
2026-03-10 03:25:42 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,9 +1,10 @@
 import google.generativeai as genai
 
 try:
-    genai.configure(api_key="AIzaSyD1YsAawa4vxrdq9OD7skuS55TuTnPVcUg")
+    genai.configure(api_key="AIzaSasdfasdfasdfg")
     model = genai.GenerativeModel('gemini-2.5-flash')
     response = model.generate_content("hello")
     print("API is active, generated:", response.text)
 except Exception as e:
     print(f"API failed: {e}")
+
